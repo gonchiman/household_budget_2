aa5f201812a34e271fea0fb4c86f4850bc2c6411
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-03-27 15:14:29 +0900

    add TransactionService

---
+++ src/transaction_service.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+from src.transaction import Transaction, TransactionType
+from src.transaction_repository import TransactionRepository
+
+
+class TransactionService:
+    def __init__(self, repository: TransactionRepository):
+        self._repository = repository
+
+    def add_transaction(
+        self,
+        transaction_date: date,
+        amount: int,
+        transaction_type: TransactionType,
+        memo: str
+    ) -> None:
+        transaction = Transaction(
+            transaction_date=transaction_date,
+            amount=amount,
+            transaction_type=transaction_type,
+            memo=memo
+        )
+        self._repository.add(transaction)
